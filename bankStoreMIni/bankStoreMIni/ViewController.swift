426de1c63b19e420c9b9682b9a3f5423428eafe4
choi bong sik <redsssl@gmail.com>
2018-06-19 19:43:44 +0900

    createProject

---
+++ bankStoreMIni/bankStoreMIni/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bankStoreMIni
+//
+//  Created by 최 봉식 on 2018. 6. 19..
+//  Copyright © 2018년 jake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
